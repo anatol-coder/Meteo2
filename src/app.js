a473f698454a789101fd560ed38ae0261a8b93f3
anatol-coder <ankawysota@gmail.com>
2024-09-01 07:36:50 +0100

    Added search functionality using API

--- src/app.js
+++ src/app.js
@@ -1,10 +1,24 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "5b407712f11dct93a10f4f1dc8e2394o";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInputElement.value;
+
+  searchCity(searchInputElement.value);
 }
-console.log(axios);
+
 let searchFormElement = document.querySelector("#search-form");
-console.log(searchFormElement);
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+searchCity("Tbilisi");
